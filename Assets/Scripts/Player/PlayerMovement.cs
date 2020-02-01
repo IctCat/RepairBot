0f6fe5168128e26efe05b50011b26f552541b6cb
IctCat <henri.hamalainen97@gmail.com>
2020-02-01 14:55:21 +0200

    Basic player control test. Graph logic

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player {
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float speed = 10f;
+        // Start is called before the first frame update
+        void Start() {
+            Debug.Log("Start!");
+        }
+
+        void Update () {
+            Vector3 pos = transform.position;
+    
+            if (Input.GetKey ("w") || Input.GetKey("up")) {
+               pos.z += speed * Time.deltaTime;
+            }
+            if (Input.GetKey ("s") || Input.GetKey("down")) {
+               pos.z -= speed * Time.deltaTime;
+            }
+            if (Input.GetKey ("d") || Input.GetKey("right")) {
+               pos.x += speed * Time.deltaTime;
+            }
+            if (Input.GetKey ("a") || Input.GetKey("left")) {
+               pos.x -= speed * Time.deltaTime;
+            }
+            if (Input.GetKey ("space")) {
+               pos.y += speed * Time.deltaTime;
+            }
+
+            transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/PowerGraph/PowerTypes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PowerGraph {
+    abstract public class PowerTypes
+    {
+        private bool working;
+        public int cost;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // TODO initializations?
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public bool IsWorking() {
+            return this.working;
+        }
+
+
+        /// <summary> Repairs the element and returns reduced resource(int)</summary>
+        /// <param name="resource">Int value</param>
+        public int Repair(int resource) {
+            this.working = true;
+            resource -= this.cost;
+            return resource;
+        }
+    }
+}
