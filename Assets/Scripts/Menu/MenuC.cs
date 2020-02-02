9035fa13a7cfc7ccb5f4b5e0d8c475d0533f89a7
IctCat <henri.hamalainen97@gmail.com>
2020-02-02 14:16:12 +0200

    PANIC

---
+++ Assets/Scripts/Menu/MenuC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuC : MonoBehaviour
+{
+    public const string MenuScene = "Menu";
+    public const string GameScene = "MainScene";
+    public const string CreditsScene = "Credits";
+
+    [SerializeField] private GameObject StartObject = null;
+    [SerializeField] private GameObject CreditsObject = null;
+
+    /*private Button StartBt;
+    private Button CreditsBt;*/
+    
+    void Awake()
+    {
+        /*StartBt = StartObject.GetComponent<Button>();
+        StartBt.onClick.AddListener(StartGame);
+        CreditsBt = CreditsObject.GetComponent<Button>();
+        CreditsBt.onClick.AddListener(PlayCredits);*/
+    }
+
+    void StartGame() {
+        SceneManager.LoadScene(GameScene);
+    }
+
+    void PlayCredits() {
+        SceneManager.LoadScene(CreditsScene);
+    }
+}
